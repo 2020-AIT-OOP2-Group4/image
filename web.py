ba77e004e7a8345294498e3479b2866d483ef9af
ryuya6104 <75057560+ryuya6104@users.noreply.github.com>
2020-12-08 15:12:19 +0900

    トップページおよびweb.py(その５)

--- web.py
+++ web.py
@@ -1,18 +1,26 @@
-from flask import Flask, request, render_template
-import random 
+from flask import Flask, request, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+import glob
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = './upload_images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+OUTPUT_FOLDER = './output_images'
+app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', message=None)
 
-@app.route('/upload' method="post")
+@app.route('/upload' method=['POST'])
 def upload():
     fs = request.files['file']
     fs.save(UPLOAD_FOLDER + '/' + secure_filename(fs.filename))
-    return render_template("index.html")
+    return render_template("index.html", message="ファイルのアップロードが完了しました。")
 
 @app.route('')
 def uploaded_list():
