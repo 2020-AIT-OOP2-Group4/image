ae06267548a1290677a1ba248f421a0ae4013338
ryuya6104 <75057560+ryuya6104@users.noreply.github.com>
2020-12-09 15:27:20 +0900

    トップページ及びweb.py(その６)

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ import glob
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = './upload_images'
+UPLOAD_FOLDER = './images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 OUTPUT_FOLDER = './output_images'
@@ -16,16 +16,35 @@ app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 def index():
     return render_template('index.html', message=None)
 
-@app.route('/upload' method=['POST'])
+@app.route('/upload', method=['POST'])
 def upload():
+    if 'file' not in request.files:
+        return render_template("index.html", message="ファイルを指定してください。")
+
     fs = request.files['file']
     fs.save(UPLOAD_FOLDER + '/' + secure_filename(fs.filename))
     return render_template("index.html", message="ファイルのアップロードが完了しました。")
 
-@app.route('')
+@app.route('/uploaded_list/')
 def uploaded_list():
+    files = glob.glob("./images/*")
+    urls = []
+    for file in files:
+        urls.append("/uploaded/" + os.path.basename(file))
+    return render_template("filelist.html", page_title="アップロードファイル", target_files=urls)
+
+
+@app.route('/grayscale_list/')
+def uprocessed_gs_list():
     pass
 
+@app.route('/canny_list/')
+def uprocessed_ca_list():
+    pass
+
+@app.route('/face_list/')
+def uprocessed_fc_list():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
